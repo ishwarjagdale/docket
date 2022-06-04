4f858ad883c30203483a216a93e32c5e4bd8c24c
Ishwar Jagdale <ishwarrules@gmail.com>
2022-06-04 22:06:07 +0530

    added api_url var

--- src/api/auth.jsx
+++ src/api/auth.jsx
@@ -4,9 +4,10 @@ import {notify} from "../components/notifier";
 axios.defaults.withCredentials = true;
 let token = null;
 let inProgress = false;
+let api_url = 'https://docket-note.herokuapp.com/';
 
 async function login(credentials, register=false) {
-    return await axios.post(register? "/register" : "/login", credentials).then((res) => {
+    return await axios.post(api_url + register? "/register" : "/login", credentials).then((res) => {
         if(res.status === 200) {
             localStorage.setItem("user", JSON.stringify(res.data));
             notify(`${register ? 'Registered' : 'Logged'} In Successfully!`, "success");
@@ -19,7 +20,7 @@ async function login(credentials, register=false) {
 }
 
 function vefyu() {
-    return axios.get("/auth/verify").then((res) => {
+    return axios.get(api_url + "/auth/verify").then((res) => {
         if(res.status === 200) {
             localStorage.setItem("user", JSON.stringify(res.data));
             if(!inProgress) {
@@ -51,7 +52,7 @@ function verifyUser() {
 }
 
 async function getNotes(noteId=null) {
-    return await axios.get(noteId ? `/api/notes/${noteId}` : "/api/notes").then((res) => {
+    return await axios.get(api_url + noteId ? `/api/notes/${noteId}` : "/api/notes").then((res) => {
         if(res.status === 200) {
             return res.data;
         }
@@ -62,7 +63,7 @@ async function getNotes(noteId=null) {
 }
 
 async function deleteNote(noteId) {
-    return await axios.delete(`/api/notes/${noteId}`).then((res) => {
+    return await axios.delete(api_url + `/api/notes/${noteId}`).then((res) => {
         return res.status === 200;
     }).catch((e) => {
         console.log(e);
@@ -71,7 +72,7 @@ async function deleteNote(noteId) {
 }
 
 async function newNote(note) {
-    return await axios.post("/api/notes", {...note}).then((res) => {
+    return await axios.post(api_url + "/api/notes", {...note}).then((res) => {
         return res.status === 200;
     }).catch((e) => {
         console.log(e);
@@ -80,7 +81,7 @@ async function newNote(note) {
 }
 
 async function logoutUser() {
-    return await axios.get("/auth/logout").then((res) => {
+    return await axios.get(api_url + "/auth/logout").then((res) => {
         return res.status === 200;
     }).catch((e) => {
         console.log(e);
